54cb07e4bc253cbcc2275ed7a15cef8b5a8dea5c
coffee-marios <mark_theo@yahoo.com>
2024-07-30 10:10:54 +0300

    Create a binary search tree using random numbers

---
+++ test.js
@@ -0,0 +1,29 @@
+import { Node, Tree, prettyPrint } from "./index.js";
+
+function randomNumber(min, max) {
+  // The maximum is exclusive and the minimum is inclusive
+  const minCeiled = Math.ceil(min);
+  const maxFloored = Math.floor(max);
+  return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled);
+}
+
+function numbersTree(num = 10) {
+  if (num > 100) return null;
+  let allData = [];
+  while (allData.length < num) {
+    const addData = randomNumber(1, 101);
+    allData.push(addData);
+    if (num === allData.length) {
+      allData = [...new Set(allData)];
+    }
+  }
+  return allData;
+}
+
+// Create a binary search tree from an array of random numbers
+
+const tree = new Tree();
+const data = numbersTree();
+tree.buildTree(data);
+const root = tree.get_root();
+prettyPrint(root);
